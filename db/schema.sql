71b59ee59a885f9e527ad6d5435f691d6491b101
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-10-19 09:35:09 -0400

    fixed schema and seed files, everything works now

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE roles (
     role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
@@ -21,7 +21,7 @@ CREATE TABLE employees (
     employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(role_id),
     FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
